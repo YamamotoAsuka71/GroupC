73ec2f01f35e27387858a949fcb6785baef5d44b
yutyann5213 <yutyanntyenn5213@icloude.com>
2023-06-20 11:37:19 +0900

    テキスト追加(未完成)

--- GroupC/Assets/Scripts/Story1.cs
+++ GroupC/Assets/Scripts/Story1.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Story1 : MonoBehaviour
 {
+    [SerializeField] UnityEngine.UI.Text textbox;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
+        textbox.text = "衝撃";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
+
+        textbox.text = "撃滅のセカンドブリット";
+        yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        yield return null;
     }
 
     // Update is called once per frame
